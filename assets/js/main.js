c84389e1bca10d907f4dcea68487419b62d8f803
John Bell <jdbell123@gmail.com>
2021-01-29 19:49:14 -0500

    added sample code. added more links. added favicon.

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// document ready statement to make sure the page fully loads before running this code.
+$(document).ready(function () {
+
+    // initialize foundation plugin.
+    $(document).foundation();
+
+});
