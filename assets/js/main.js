f44a92e5c538998407e1f9d324854423e941de8c
Devante Johns <johnsdevante74@gmail.com>
2021-02-02 20:56:36 -0500

    incorporated the GIF API to the main.js section

--- assets/js/main.js
+++ assets/js/main.js
@@ -450,3 +450,48 @@ $(document).ready(function () {
   // ************************************************************************************
   // ************************************************************************************
 });
+
+// ************************************************************************************
+  // ************************************************************************************
+  // ************************************************************************************
+  // **  
+  // **  <start> GIF Button <start>
+  // **  
+  // ************************************************************************************
+  // ************************************************************************************
+  // ***************************************************************************
+  // ************************************************************************************
+  
+  
+      $(".gifs").on("click", function() {
+        var teamSearch = "Chelsea"
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + "soccer "   +
+          teamSearch + "&api_key=dc6zaTOxFJmzC&limit=10";
+  console.log("this works")
+  
+       $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+          .then(function(response) {
+            console.log(response)
+            var results = response.data;
+  
+            for (var i = 0; i < results.length; i++) {
+              var gifDiv = $("<div>");
+  
+              var rating = results[i].rating;
+  
+              var p = $("<p>").text("Rating: " + rating);
+  
+              var personImage = $("<img>");
+              personImage.attr("src", results[i].images.fixed_height.url);
+  
+              gifDiv.prepend(p);
+              gifDiv.prepend(personImage);
+  
+              $("#gifs-appear-here").prepend(gifDiv);
+            }
+         });
+      });
+  
